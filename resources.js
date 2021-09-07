dda7ad232df6598b125610177a5fdb4481159ac6
Brian Whitney <44126568+brianr-w@users.noreply.github.com>
2021-09-07 09:54:15 -0700

    Add files via upload

---
+++ resources.js
@@ -0,0 +1,99 @@
+function navtoAbout(){
+    console.log("Nav to About");
+    var home = document.getElementById("home");
+    var about = document.getElementById("about");
+    var ideas = document.getElementById("ideas");
+    var clubs = document.getElementById("clubs");
+    var store = document.getElementById("store");
+
+    var aboutMenu = document.getElementById("aboutMenu");
+    var ideasMenu = document.getElementById("ideasMenu");
+    var clubsMenu = document.getElementById("clubsMenu");
+    var storeMenu = document.getElementById("storeMenu");
+
+    home.style.visibility = "hidden";
+    about.style.visibility = "visible";
+    ideas.style.visibility = "hidden";
+    clubs.style.visibility = "hidden";
+    store.style.visibility = "hidden";
+
+    aboutMenu.style.backgroundColor = "#DFE8EF";
+    ideasMenu.style.backgroundColor = "#F7F7FA";
+    clubsMenu.style.backgroundColor = "#F7F7FA";
+    storeMenu.style.backgroundColor = "#F7F7FA";
+}
+
+function navtoIdeas(){
+    console.log("Nav to Ideas");
+    var home = document.getElementById("home");
+    var about = document.getElementById("about");
+    var ideas = document.getElementById("ideas");
+    var clubs = document.getElementById("clubs");
+    var store = document.getElementById("store");
+
+    var aboutMenu = document.getElementById("aboutMenu");
+    var ideasMenu = document.getElementById("ideasMenu");
+    var clubsMenu = document.getElementById("clubsMenu");
+    var storeMenu = document.getElementById("storeMenu");
+
+    home.style.visibility = "hidden";
+    about.style.visibility = "hidden";
+    ideas.style.visibility = "visible";
+    clubs.style.visibility = "hidden";
+    store.style.visibility = "hidden";
+
+    aboutMenu.style.backgroundColor = "#F7F7FA";
+    ideasMenu.style.backgroundColor = "#DFE8EF";
+    clubsMenu.style.backgroundColor = "#F7F7FA";
+    storeMenu.style.backgroundColor = "#F7F7FA";
+}
+
+function navtoClubs(){
+    console.log("Nav to Clubs");
+    var home = document.getElementById("home");
+    var about = document.getElementById("about");
+    var ideas = document.getElementById("ideas");
+    var clubs = document.getElementById("clubs");
+    var store = document.getElementById("store");
+
+    var aboutMenu = document.getElementById("aboutMenu");
+    var ideasMenu = document.getElementById("ideasMenu");
+    var clubsMenu = document.getElementById("clubsMenu");
+    var storeMenu = document.getElementById("storeMenu");
+
+    home.style.visibility = "hidden";
+    about.style.visibility = "hidden";
+    ideas.style.visibility = "hidden";
+    clubs.style.visibility = "visible";
+    store.style.visibility = "hidden";
+
+    aboutMenu.style.backgroundColor = "#F7F7FA";
+    ideasMenu.style.backgroundColor = "#F7F7FA";
+    clubsMenu.style.backgroundColor = "#DFE8EF";
+    storeMenu.style.backgroundColor = "#F7F7FA";
+}
+
+function navtoStore(){
+    console.log("Nav to Store");
+    var home = document.getElementById("home");
+    var about = document.getElementById("about");
+    var ideas = document.getElementById("ideas");
+    var clubs = document.getElementById("clubs");
+    var store = document.getElementById("store");
+
+    var aboutMenu = document.getElementById("aboutMenu");
+    var ideasMenu = document.getElementById("ideasMenu");
+    var clubsMenu = document.getElementById("clubsMenu");
+    var storeMenu = document.getElementById("storeMenu");
+
+    home.style.visibility = "hidden";
+    about.style.visibility = "hidden";
+    ideas.style.visibility = "hidden";
+    clubs.style.visibility = "hidden";
+    store.style.visibility = "visible";
+
+    aboutMenu.style.backgroundColor = "#F7F7FA";
+    ideasMenu.style.backgroundColor = "#F7F7FA";
+    clubsMenu.style.backgroundColor = "#F7F7FA";
+    storeMenu.style.backgroundColor = "#DFE8EF";
+}
